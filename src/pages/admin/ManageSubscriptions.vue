a35dfb315c2479345799c112115a8ba911e1609a
Darko <darko.simonovski@bestisoft.com>
2019-03-08 13:06:11 +0100

    Finished template for subs management

--- src/pages/admin/ManageSubscriptions.vue
+++ src/pages/admin/ManageSubscriptions.vue
@@ -4,7 +4,7 @@
       .flex-1
       .flex-col.flex-2
         h1 Manage subscriptions
-        .p5-bot
+        .p15-bot
         h2 Subscribers
         br
         v-data-table.elevation-1(:items="subscribers" :headers="subscriberHeaders")
@@ -14,7 +14,25 @@
             td {{ props.item.email }}
             td {{ props.item.type | subType }}
         .p30-top
-          p New subscriber fields here...
+        hr(style="border-top:0.8px solid white")
+        .p15-top
+          h2 Add new subscriber
+          br
+          v-form(ref="subscriberForm")
+            v-text-field(v-model="subscriber.name" :rules="[(v) => !!v || 'Name is required']" required placeholder="Name" solo)
+            v-text-field(v-model="subscriber.surname" :rules="[(v) => !!v || 'Surname is required']" required placeholder="Surname" solo)
+            v-text-field(v-model="subscriber.email" :rules="[(v) => !!v || 'Email is required']" required type="email" placeholder="Email" solo)
+            v-select(v-model="subscriber.type" :items="subscriberTypes" :rules="[(v) => v != null || 'Type is required']" required placeholder="Type" solo)
+          v-btn(@click="submitSubscriber" color="primary") Submit
+        .p30-top
+        hr(style="border-top:0.8px solid white")
+        .p15-top
+          h2 Add new subscription
+          br
+          v-form(ref="subscriptionForm")
+            v-select(v-model="subscription.entityId" :items="subscriberTypes" :rules="[(v) => v != null || 'Subscriber is required']" required placeholder="Subscriber" solo)
+            v-select(v-model="subscriber.pkgId" :items="subscriberTypes" :rules="[(v) => v != null || 'Package is required']" required placeholder="Package" solo)
+          v-btn(@click="submitSubscription" color="primary") Submit
       .flex-1
 </template>
 
@@ -28,12 +46,18 @@ export default {
   data() {
     return {
       subscribers: [],
+      subscriber: {},
       subscriberHeaders: [
         { text: 'Name', value: 'name' },
         { text: 'Surname', value: 'surname' },
         { text: 'Email', value: 'email' },
         { text: 'Type', value: 'type' }
       ],
+      subscriberTypes: [
+        { text: 'Business', value: 0 },
+        { text: 'Consumer', value: 1 }
+      ],
+      subscription: {}
     }
   },
   methods: {
@@ -42,6 +66,16 @@ export default {
         .then((res) => {
           this.subscribers = res.data
         })
+    },
+    submitSubscriber() {
+      const subscriberValid = this.$refs.subscriberForm.validate()
+      if (!subscriberValid) return
+      console.log(this.subscriber)
+    },
+    submitSubscription() {
+      const subscriptionValid = this.$refs.subscriptionForm.validate()
+      if (!subscriptionValid) return
+      console.log(this.subscription)
     }
   }
 }
