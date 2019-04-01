e59798d629ead4825401108bc6858b86c3711856
Darko <darko.simonovski@bestisoft.com>
2019-04-01 21:33:05 +0200

    Separate component for promo images

---
+++ src/components/PromoImage.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  v-badge(overlap)
+    div(slot="badge" v-if="promoImage.featured")
+      v-icon(small color="white") fas fa-star
+    .flex-col.align-center
+      v-avatar(tile size="100")
+        img(v-if="!slotted" :src="getImageUrl(promoImage.imageUrl)")
+        slot(v-else)
+      span {{ promoImage.desc }}
+</template>
+
+<script>
+const promoImageBase = 'https://thatsmontreal.ca:8000/static/promo' // pls temp
+
+export default {
+  props: {
+    promoImage: {
+      type: Object,
+      required: true
+    },
+    slotted: Boolean
+  },
+  methods: {
+    getImageUrl(imageName) {
+      if (imageName.includes('http')) return imageName
+      return `${promoImageBase}/${imageName}`
+    }
+  }
+}
+</script>
--- src/components/PromoItem.vue
+++ src/components/PromoItem.vue
@@ -1,22 +1,17 @@
 <template lang="pug">
   v-card
-    .p10
+    .p15
       h1 {{ promo.title }}
       h3 {{ promo.desc }}
       br
       v-checkbox(v-model="promo.active" hide-details disabled label="Active")
       .flex-row.p5
-        .p5(v-for="promoImage in promo.promoImages" :key="promoImage.id")
-          v-badge(overlap)
-            div(slot="badge" v-if="promoImage.featured")
-              v-icon(small color="white") fas fa-star
-            v-avatar(tile size="100")
-              img(:src="getImageUrl(promoImage.imageUrl)")
+        .p15(v-for="promoImage in promo.promoImages" :key="promoImage.id")
+          PromoImage(:promoImage="promoImage")
 </template>
 
 <script>
-
-const promoImageBase = 'https://thatsmontreal.ca:8000/static/promo' // pls temp
+import PromoImage from '@/components/PromoImage'
 
 export default {
   props: {
@@ -25,11 +20,8 @@ export default {
       required: true
     }
   },
-  methods: {
-    getImageUrl(imageName) {
-      if (imageName.includes('http')) return imageName
-      return `${promoImageBase}/${imageName}`
-    }
+  components: {
+    PromoImage
   }
 }
 </script>
