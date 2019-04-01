17fab4b4c1b62a03fb5c95ddc4ce8da9b79ee6fe
Darko <darko.simonovski@bestisoft.com>
2019-04-01 21:33:34 +0200

    A simple youtube video container

---
+++ src/components/YoutubeContainer.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+    iframe(width="420" height="315" :src="getEmbedUrl")
+</template>
+
+<script>
+import url from 'url'
+
+export default {
+  props: {
+    videoUrl: String
+  },
+  computed: {
+    getEmbedUrl() {
+      const urlParts = url.parse(this.videoUrl, true)
+      const query = urlParts.query
+      return `https://www.youtube.com/embed/${query.v}`
+    }
+  }
+}
+</script>
+
