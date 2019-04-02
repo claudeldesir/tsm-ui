4508b5574561764395a6270cce5872b444f7a7c0
Darko Simonovski <darko.simonovski@hotmail.com>
2019-04-03 00:48:39 +0200

    We no longer need this (good to know)

--- src/components/PromoImage.vue
+++ src/components/PromoImage.vue
@@ -4,13 +4,12 @@
       v-icon(small color="white") fas fa-star
     .flex-col.align-center
       v-avatar(tile size="100")
-        img(v-if="!slotted" :src="getImageUrl(promoImage.imageUrl)")
+        img(v-if="!slotted" :src="promoImage.imageUrl")
         slot(v-else)
       span {{ promoImage.desc }}
 </template>
 
 <script>
-const promoImageBase = 'https://thatsmontreal.ca:8000/static/promo' // pls temp
 
 export default {
   props: {
@@ -19,12 +18,6 @@ export default {
       required: true
     },
     slotted: Boolean
-  },
-  methods: {
-    getImageUrl(imageName) {
-      if (imageName.includes('http')) return imageName
-      return `${promoImageBase}/${imageName}`
-    }
   }
 }
 </script>
