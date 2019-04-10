60edd71bf8d50e753c9e34d8425c2f0f28a3d425
Darko <darko.simonovski@bestisoft.com>
2019-04-10 22:08:48 +0200

    Created layout for ad details (maps missing)

--- src/components/spotit/AdDetails.vue
+++ src/components/spotit/AdDetails.vue
@@ -1,15 +1,65 @@
 <template lang="pug">
   div
-    span Ad: {{ ad }}
+    h2 {{ ad.title }}
+    .fs14 Posted&nbsp;
+      timeago(:datetime="ad.createdAt" :autoUpdate="true")
+    br
+    .flex-row
+      .w66.p10.tiny-border
+        ImageItem(:image="currentImage" original)
+      .w33.p10
+        .flex-row.w100.justify-center
+          img(src="http://staticmapmaker.com/img/google.png")
+    .flex-row.m5
+      .p5(v-for="image in ad.adImages" :key="image.id" :class="{'tiny-border': isSelected(image.id)}")
+        ImageItem(:image="image" :full="false" @selected="changeCurrentImage(image)")
+    br
+    .flex-row
+      .fs20.tiny-border.p20(style="white-space:pre-wrap;") {{ ad.content }}
+    br
+    .flex-row
+      .tiny-border.p20.fs15
+        .flex-row.p5
+          h4 Contact name:&nbsp;
+          span {{ ad.contactName }}
+        .flex-row.p5
+          h4 Phone number:&nbsp;
+          span(v-if="ad.phone") {{ ad.phone }}
+          span.italic(v-else) not visible
+        .flex-row.p5
+          h4 Email address:&nbsp;
+          span(v-if="ad.email") {{ ad.email }}
+          span.italic(v-else) not visible
 </template>
 
 <script>
+import ImageItem from '@/components/ImageItem'
+
 export default {
   props: {
     ad: {
       type: Object,
       required: true
     }
+  },
+  created() {
+    this.currentImage = this.ad.adImages[0]
+  },
+  data() {
+    return {
+      currentImage: {}
+    }
+  },
+  methods: {
+    changeCurrentImage(image) {
+      if (image) this.currentImage = image
+    },
+    isSelected(imageId) {
+      return this.currentImage.id === imageId
+    }
+  },
+  components: {
+    ImageItem
   }
 }
 </script>
