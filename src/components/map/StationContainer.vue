936e76efa170948d0a48f72407d8a8793cca93a9
Darko <darko.simonovski@bestisoft.com>
2019-04-15 00:25:23 +0200

    Moved station carousel and options into a separate component

---
+++ src/components/map/StationContainer.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  .flex-col.align-center
+    .p5
+      h3 {{ station.title }}
+    .p5
+      span {{ station.desc }}
+    .p5-ver.w100
+      carousel(:perPage="1" paginationActiveColor="#fff" paginationColor="#555" @page-change="transitionCount += 1")
+        slide(v-for="mediaItem in media" :key="mediaItem.id")
+          MediaContainer(:mediaItem="mediaItem" :transitionCount="transitionCount")
+    .p20
+      button.btn.btn-outline-light.promoBtn
+        i.fas.fa-gift
+        br
+        span GET A FREE GIFT!
+    span Share with your friends!
+    .flex-row.p5
+      social-sharing(url="https://google.com" media="https://google.com" title="this is title" description="this is desc" quote="this is quote" inline-template)
+        .flex-row.center
+          network(network="facebook")
+            i.p10.fs30.fab.fa-facebook.pointer
+          network(network="twitter")
+            i.p10.fs30.fab.fa-twitter.pointer
+          network(network="googleplus")
+            i.p10.fs30.fab.fa-google-plus.pointer
+          network(network="pinterest")
+            i.p10.fs30.fab.fa-pinterest.pointer
+          network(network="linkedin")
+            i.p10.fs30.fab.fa-linkedin.pointer
+</template>
+
+<script>
+import MediaContainer from '@/components/map/MediaContainer'
+
+export default {
+  props: {
+    station: {
+      type: Object,
+      required: true
+    },
+    media: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      transitionCount: 0 // okaay... *but Nikola did this!*
+    }
+  },
+  components: {
+    MediaContainer
+  }
+}
+</script>
+
--- src/components/map/StationPopup.vue
+++ src/components/map/StationPopup.vue
@@ -4,42 +4,15 @@
       div(v-if="loaded")
         .flex-row.p10.justify-end.close-popup.pointer(@click="$emit('close')")
           i.fs20.p5.fas.fa-times
-        .flex-col.align-center
-          .p5
-            h3 {{ station.title }}
-          .p5
-            span {{ station.desc }}
-          .p5-ver.w100
-            carousel(:perPage="1" paginationActiveColor="#fff" paginationColor="#555" @page-change="transitionCount += 1")
-              slide(v-for="mediaItem in media" :key="mediaItem.id")
-                MediaContainer(:mediaItem="mediaItem" :transitionCount="transitionCount")
-          .p20
-            button.btn.btn-outline-light.promoBtn
-              i.fas.fa-gift
-              br
-              span GET A FREE GIFT!
-          span Share with your friends!
-          .flex-row.p5
-            social-sharing(url="https://google.com" media="https://google.com" title="this is title" description="this is desc" quote="this is quote" inline-template)
-              .flex-row.center
-                network(network="facebook")
-                  i.p10.fs30.fab.fa-facebook.pointer
-                network(network="twitter")
-                  i.p10.fs30.fab.fa-twitter.pointer
-                network(network="googleplus")
-                  i.p10.fs30.fab.fa-google-plus.pointer
-                network(network="pinterest")
-                  i.p10.fs30.fab.fa-pinterest.pointer
-                network(network="linkedin")
-                  i.p10.fs30.fab.fa-linkedin.pointer
-      Loading(v-else)
+        StationContainer(:station="station" :media="media")
+      Loading.p10(v-else)
 </template>
 
 <script>
 import { mapPoints } from '@/data/map-points'
 import Api from '@/services/api'
 
-import MediaContainer from '@/components/map/MediaContainer'
+import StationContainer from '@/components/map/StationContainer'
 
 export default {
   props: {
@@ -64,8 +37,7 @@ export default {
     return {
       station: {},
       media: [],
-      loaded: false,
-      transitionCount: 0 // okaay...
+      loaded: false
     }
   },
   methods: {
@@ -89,7 +61,7 @@ export default {
     }
   },
   components: {
-    MediaContainer
+    StationContainer
   }
 }
 </script>
