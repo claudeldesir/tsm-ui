383b85e68f8fbdbc37772c4e7e42ab873c528c1b
Darko Simonovski <darko.simonovski@hotmail.com>
2019-04-14 14:54:07 +0200

    Moved all the youtube related matters in the youtube container

--- src/components/YoutubeContainer.vue
+++ src/components/YoutubeContainer.vue
@@ -1,20 +1,50 @@
 <template lang="pug">
   div
-    iframe(width="420" height="315" :src="getEmbedUrl")
+    youtube(ref="youtubeEl" :player-vars="playerVars" :resize="resize" :video-id="getVideoId()" @ready="fixSize")
 </template>
 
 <script>
-import url from 'url'
+import Vue from 'vue'
 
 export default {
   props: {
     videoUrl: String
   },
-  computed: {
-    getEmbedUrl() {
-      const urlParts = url.parse(this.videoUrl, true)
-      const query = urlParts.query
-      return `https://www.youtube.com/embed/${query.v}`
+  data() {
+    return {
+      resize: true,
+      playerVars: {
+        showinfo: 0,
+        controls: 0,
+        autohide: 1
+      },
+    }
+  },
+  methods: {
+    fixSize() {
+      this.resize = false
+      Vue.nextTick(() => {
+        this.resize = true
+      })
+    },
+    getVideoId() {
+      return this.$youtube.getIdFromUrl(this.videoUrl)
+    },
+    toggleVideo(forceStop) {
+      const youtubeEl = this.$refs.youtubeEl
+      const player = youtubeEl.player
+
+      const validStates = [-1, 2, 5]
+      const stateP = player.getPlayerState()
+      stateP.then((state) => {
+        if (forceStop) {
+          youtubeEl.player.stopVideo()
+        } else if (state === 1) {
+          youtubeEl.player.pauseVideo()
+        } else if (validStates.includes(state)) {
+          youtubeEl.player.playVideo()
+        }
+      })
     }
   }
 }
--- src/components/map/MediaContainer.vue
+++ src/components/map/MediaContainer.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
   div
-    youtube(v-if="mediaItem.type === 0" ref="youtubeEl" :player-vars="playerVars" :resize="resize" :video-id="getMediaSrc()" @ready="fixSize")
+    YoutubeContainer(v-if="mediaItem.type === 0" ref="ytContainer" :videoUrl="mediaItem.url")
     .p5.tiny-border.text-center(v-else) Unsupported media type
     .w100.h100.absolute(style="top:0;z-index:99999;" @click="toggleVideo(false)")
 </template>
 
 <script>
-import Vue from 'vue'
-import url from 'url'
+import YoutubeContainer from '@/components/YoutubeContainer'
 
 export default {
   props: {
@@ -24,49 +23,16 @@ export default {
   },
   data() {
     return {
-      resize: true,
-      playerVars: {
-        showinfo: 0,
-        controls: 0,
-        autohide: 1
-      },
       isDrag: false
     }
   },
   methods: {
-    getMediaSrc() {
-      const mediaUrl = this.mediaItem.url
-      const type = this.mediaItem.type
-
-      if (type === 0) {
-        const urlParts = url.parse(mediaUrl, true)
-        const query = urlParts.query
-        return query.v
-      }
-      return mediaUrl
-    },
-    fixSize() {
-      this.resize = false
-      Vue.nextTick(() => {
-        this.resize = true
-      })
-    },
     toggleVideo(forceStop) {
-      const youtubeEl = this.$refs.youtubeEl
-      const player = youtubeEl.player
-
-      const validStates = [-1, 2, 5]
-      const stateP = player.getPlayerState()
-      stateP.then((state) => {
-        if (forceStop) {
-          youtubeEl.player.stopVideo()
-        } else if (state === 1) {
-          youtubeEl.player.pauseVideo()
-        } else if (validStates.includes(state)) {
-          youtubeEl.player.playVideo()
-        }
-      })
+      this.$refs.ytContainer.toggleVideo(forceStop)
     }
+  },
+  components: {
+    YoutubeContainer
   }
 }
 </script>
