939604bdd6d523969e93ab4b986d43597653f0d6
Darko <darko.simonovski@bestisoft.com>
2019-04-18 14:52:54 +0200

    Added all map points!

--- src/data/map-points.js
+++ src/data/map-points.js
@@ -1,80 +1,357 @@
-const mapPoints = [{
-  id: 40,
+const mapPointsObj = [{ // here is orange
+  id: 1,
   size: 1,
-  top: 54.4,
-  left: 23.25
+  top: 8.9,
+  left: 11.7
 }, {
-  id: 44,
+  id: 2,
   size: 1,
-  top: 70.7,
+  top: 8.9,
+  left: 20.4
+}, {
+  id: 3,
+  size: 1,
+  top: 8.9,
+  left: 29.3
+}, {
+  id: 4,
+  size: 1,
+  top: 15.3,
+  left: 35.1
+}, {
+  id: 5,
+  size: 1,
+  top: 18.5,
   left: 37.9
 }, {
-  id: 47,
+  id: 6,
   size: 1,
-  top: 70.7,
-  left: 49.6
+  top: 21.8,
+  left: 40.8
 }, {
-  id: 48,
+  id: 7,
   size: 1,
-  top: 67.4,
-  left: 52.5
+  top: 25.0,
+  left: 43.8
 }, {
-  id: 53,
+  id: 8,
   size: 2,
-  top: 47.47,
-  left: 63.75,
-  boxShadow: '0 0 12px 4px #fff,0 0 10px 3px #f0f,0 0 20px 8px #0ff'
+  top: 27.85,
+  left: 46.35
 }, {
-  id: 54,
-  size: 1,
-  top: 43.5,
-  left: 60.25
-}, {
-  id: 55,
-  size: 1,
-  top: 40.53,
-  left: 57.57
-}, {
-  id: 58,
+  id: 9,
   size: 1,
   top: 31.5,
   left: 49.5
 }, {
-  id: 71,
+  id: 10,
   size: 1,
-  top: 47.85,
-  left: 40.9
+  top: 34.5,
+  left: 52.3
 }, {
-  id: 78,
+  id: 11,
+  size: 1,
+  top: 37.4,
+  left: 54.9
+}, {
+  id: 12,
+  size: 1,
+  top: 40.53,
+  left: 57.57
+}, {
+  id: 13,
+  size: 1,
+  top: 43.5,
+  left: 60.25
+}, {
+  id: 14,
+  size: 2,
+  top: 47.47,
+  left: 63.75
+}, {
+  id: 15,
+  size: 1,
+  top: 54.4,
+  left: 64.2
+}, {
+  id: 16,
+  size: 1,
+  top: 57.6,
+  left: 61.3
+}, {
+  id: 17,
+  size: 1,
+  top: 60.8,
+  left: 58.3
+}, {
+  id: 18,
+  size: 1,
+  top: 64.1,
+  left: 55.5
+}, {
+  id: 19,
+  size: 1,
+  top: 67.4,
+  left: 52.6
+}, {
+  id: 20,
+  size: 1,
+  top: 70.7,
+  left: 49.6
+}, {
+  id: 21,
+  size: 2,
+  top: 73.55,
+  left: 46.27
+}, {
+  id: 22,
+  size: 1,
+  top: 74.0,
+  left: 40.8
+}, {
+  id: 23,
+  size: 1,
+  top: 70.7,
+  left: 37.9
+}, {
+  id: 24,
+  size: 1,
+  top: 67.5,
+  left: 35.0
+}, {
+  id: 25,
+  size: 2,
+  top: 63.67,
+  left: 31.75
+}, {
+  id: 26,
+  size: 1,
+  top: 61.0,
+  left: 29.2
+}, {
+  id: 27,
+  size: 1,
+  top: 57.6,
+  left: 26.3
+}, {
+  id: 28,
+  size: 1,
+  top: 54.4,
+  left: 23.25
+}, {
+  id: 29,
+  size: 1,
+  top: 51.1,
+  left: 20.4
+}, {
+  id: 30,
+  size: 1,
+  top: 48.0,
+  left: 17.55
+}, {
+  id: 31,
+  size: 1,
+  top: 44.65,
+  left: 14.7
+}, { // here is blue
+  id: 32,
   size: 1,
   top: 18.5,
   left: 55.5
 }, {
-  id: 93,
+  id: 33,
+  size: 1,
+  top: 21.8,
+  left: 52.5
+}, {
+  id: 34,
+  size: 1,
+  top: 25.0,
+  left: 49.6
+}, {
+  id: 35,
+  size: 1,
+  top: 31.5,
+  left: 43.8
+}, {
+  id: 36,
+  size: 1,
+  top: 35.0,
+  left: 40.9
+}, {
+  id: 37,
+  size: 1,
+  top: 41.4,
+  left: 40.9
+}, {
+  id: 38,
+  size: 1,
+  top: 47.9,
+  left: 40.9
+}, {
+  id: 39,
+  size: 1,
+  top: 54.3,
+  left: 40.8
+}, {
+  id: 40,
   size: 1,
   top: 57.6,
-  left: 55.5
+  left: 38.0
 }, {
-  id: 95,
+  id: 41,
   size: 1,
-  top: 51.1,
-  left: 61.3
-}, {
-  id: 97,
+  top: 60.9,
+  left: 35.1
+}, { // here is green
+  id: 42,
   size: 1,
-  top: 43.7,
-  left: 67.9
+  top: 8.8,
+  left: 87.5
 }, {
-  id: 102,
+  id: 43,
+  size: 1,
+  top: 12,
+  left: 84.7
+}, {
+  id: 44,
+  size: 1,
+  top: 15.3,
+  left: 81.7
+}, {
+  id: 45,
+  size: 1,
+  top: 18.5,
+  left: 78.7
+}, {
+  id: 46,
+  size: 1,
+  top: 21.9,
+  left: 75.9
+}, {
+  id: 47,
+  size: 1,
+  top: 25.2,
+  left: 78.6
+}, {
+  id: 48,
   size: 1,
   top: 28.4,
   left: 75.9
 }, {
+  id: 49,
+  size: 1,
+  top: 31.6,
+  left: 73.0
+}, {
+  id: 50,
+  size: 1,
+  top: 34.9,
+  left: 70.0
+}, {
   id: 51,
   size: 1,
-  top: 57.6,
+  top: 38.3,
+  left: 72.9
+}, {
+  id: 52,
+  size: 1,
+  top: 40.8,
+  left: 70.5
+}, {
+  id: 53,
+  size: 1,
+  top: 43.7,
+  left: 68.0
+}, {
+  id: 54,
+  size: 1,
+  top: 51.1,
   left: 61.3
+}, {
+  id: 55,
+  size: 1,
+  top: 54.4,
+  left: 58.4
+}, {
+  id: 56,
+  size: 1,
+  top: 57.6,
+  left: 55.5
+}, {
+  id: 57,
+  size: 1,
+  top: 60.9,
+  left: 52.5
+}, {
+  id: 58,
+  size: 1,
+  top: 64.1,
+  left: 49.6
+}, {
+  id: 59,
+  size: 1,
+  top: 67.4,
+  left: 46.8
+}, {
+  id: 60,
+  size: 1,
+  top: 77.4,
+  left: 49.6
+}, {
+  id: 61,
+  size: 1,
+  top: 80.5,
+  left: 52.4
+}, {
+  id: 62,
+  size: 1,
+  top: 83.8,
+  left: 49.6
+}, {
+  id: 63,
+  size: 1,
+  top: 86.8,
+  left: 46.8
+}, {
+  id: 64,
+  size: 1,
+  top: 84.0,
+  left: 43.6
+}, {
+  id: 65,
+  size: 1,
+  top: 86.9,
+  left: 40.9
+}, {
+  id: 66,
+  size: 1,
+  top: 90.15,
+  left: 38.0
+}, { // here is yellow
+  id: 67,
+  size: 1,
+  top: 64.1,
+  left: 49.6
+}, {
+  id: 68,
+  size: 1,
+  top: 49.0,
+  left: 74.4
+}, {
+  id: 69,
+  size: 1,
+  top: 50.0,
+  left: 81.3
 }]
+
+const mapPoints = mapPointsObj.map((mapPoint) => {
+  const mapPointNew = { ...mapPoint }
+  if (mapPointNew.size === 2) {
+    mapPointNew.boxShadow = '0 0 12px 4px #fff,0 0 10px 3px #f0f,0 0 20px 8px #0ff'
+  }
+  return mapPointNew
+})
 
 const pointSizes = [{
   height: 1,
